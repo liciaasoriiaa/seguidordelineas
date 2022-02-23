6e0de12c2bf611934daacf3b0a0dda1f76daae02
liciaasoriiaa <99322427+liciaasoriiaa@users.noreply.github.com>
2022-02-23 10:15:50 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-
+basic.forever(function () {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+    } else {
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0) {
+        	
+        }
+    }
+})
